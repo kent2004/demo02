80e1f662a253a1e1ca31c9dcaa067e2d3b01fd09
kent2004 <44199950+kent2004@users.noreply.github.com>
2019-05-24 14:33:28 +0200

    Update install_apacheRH.sh

--- install_apacheRH.sh
+++ install_apacheRH.sh
@@ -1,1 +1,7 @@
-yum install apache
+yum install -y apache
+
+# write some HTML
+echo \<center\>\<h1\>My Demo App for UNIX TechDays\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
+
+# restart Apache
+apachectl restart
